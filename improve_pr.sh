b9243c4af7923c371e83fd88e56a69ff5a0df5a2
Leslie Wang <qiwa@pensando.io>
2025-03-18 12:39:36 -0500

    add my change
    Signed-off-by: Leslie Wang <qiwa@pensando.io>

---
+++ improve_pr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+set -x
+sudo docker run --rm -it -p 8000:8000 -e LOG_LEVEL=DEBUG -e GITHUB_TOKEN=$GITHUB_TOKEN -v $(pwd)/config.yaml:/app/config.yaml -v $(pwd)/pr_agent/settings/configuration_my.toml:/app/pr_agent/settings/configuration.toml pr-agent --pr_url=$1 improve_code
